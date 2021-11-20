5d5bd5f6e7a4ebabc0f5f97f18a2fe9505d4155a
karina <vargasriosk@gmail.com>
2021-11-20 18:09:40 -0500

    Probando desde el admin

--- affiliatesApp/models/affiliates.py
+++ affiliatesApp/models/affiliates.py
@@ -13,14 +13,14 @@ class Affiliates(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField('Name', max_length = 30)
     lastname = models.CharField('Lastname', max_length = 30)
-    document= DOCUMENT_TYPE_CHOICES,
-    document_number = models.CharField('DocumentNumber', max_length = 30)
+    document=  models.CharField('Document', max_length = 30, choices=DOCUMENT_TYPE_CHOICES)
+    document_number = models.CharField('Document Number', max_length = 30)
     email = models.EmailField('Email', max_length = 100)    
-    phone = PhoneNumberField()
+    phone = models.CharField('Phone', max_length = 13)
     city = models.CharField('City', max_length = 30)
     address = models.CharField('Address', max_length = 30)
-    created = models.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField('Creation Date',auto_now_add=True)
+    updated = models.DateTimeField('Updated Date', auto_now=True)
 
     def __str__(self):
-        return self.id + self.name + self.lastname + self.document + self.document_number + self.email + self.phone + self.city + self.address + self.created + self.updated
+        return self.name
