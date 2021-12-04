688c07c797d0424fc9644f14d50939fe4c14cff1
karina <vargasriosk@gmail.com>
2021-12-04 17:36:58 -0500

    Segunda guia api y creacion dockerfile

---
+++ src/server.js
@@ -0,0 +1,5 @@
+module.exports = {
+  auth_api_url: "https://eps-authms.herokuapp.com/",
+  affiliates_api_url: "https://affiliates-ms-be.herokuapp.com/",
+  covidSurvey_api_url: "",
+};
---
+++ src/typeDefs/affiliates_type_defs.js
@@ -0,0 +1,14 @@
+const { gql } = require("apollo-server");
+
+const accountTypeDefs = gql`
+  type Account {
+    username: String!
+    balance: Int!
+    lastChange: String!
+  }
+  extend type Query {
+    accountByUsername(username: String!): Account
+  }
+`;
+
+module.exports = accountTypeDefs;
