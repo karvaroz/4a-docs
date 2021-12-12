8d58df99866e99ed6cd6ff03898c6850cf26f4e2
AngeleLindarteL <angelgamerlolwtf@gmail.com>
2021-12-12 01:13:52 -0500

    fix in all affiliates view

--- affiliatesProject/urls.py
+++ affiliatesProject/urls.py
@@ -5,7 +5,7 @@ from affiliatesApp import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("affiliates/", views.AffiliatesCreateView.as_view()), 
-    path("affiliates-list/", views.AffiliatesCreateView.as_view()), 
+    path("affiliates-list/", views.AffiliatesListView.as_view()), 
     path("affiliates/<int:pk>/", views.AffiliatesDetailView.as_view()), 
     path("affiliates-update/<int:pk>/", views.AffiliatesUpdateView.as_view()), 
     path('affiliates-delete/<int:pk>/', views.AffiliatesDeleteView.as_view())
